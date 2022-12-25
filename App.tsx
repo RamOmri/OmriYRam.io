d99f590055543cee1cfc0fd30fa32a140167edc3
Omri Y Ram <Omriyram@gmail.com>
2022-12-25 21:08:13 +0100

    switches to yarn

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Dimensions, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { HomeTabs } from "./src/router";
 import { COLORS } from "./src/styles";
 
